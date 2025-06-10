701b9e18171ac895df164046ed4fc6bf5d686220
Utsv31 <utsavjha31@gmail.com>
2025-06-10 09:34:18 +0530

    data transformation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -62,11 +62,6 @@ def load_object(file_path: str, ) -> object:
         raise NetworkSecurityException(e, sys) from e
     
 def load_numpy_array_data(file_path: str) -> np.array:
-    """
-    load numpy array data from file
-    file_path: str location of file to load
-    return: np.array data loaded
-    """
     try:
         with open(file_path, "rb") as file_obj:
             return np.load(file_obj)
